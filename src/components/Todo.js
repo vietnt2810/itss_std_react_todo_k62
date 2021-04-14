fc6159d03d9c8a272ae08f0065822391546d50eb
vietnt2810 <viet.nt2810@gmail.com>
2021-04-14 12:10:46 +0700

    Add new toDo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -37,9 +37,14 @@ function Todo() {
     putItems(newItems);
   };
 
+  const handleAdd = (text) => {
+    putItems([...items, { key: getKey(), text, done: false }]);
+  };
+
   return (
     <div className="panel">
       <div className="panel-heading">ITSS ToDoアプリ</div>
+      <Input onAdd={handleAdd} />
       {items.map((item) => (
         <TodoItem key={item.key} item={item} onClick={handleCheck} />
       ))}
