f42c66a0489b89d0ff1f1ffab061cfc42ea2c65b
vietnt2810 <viet.nt2810@gmail.com>
2021-04-14 12:17:41 +0700

    Save toDo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,13 @@ import useStorage from "../hooks/storage";
 import { getKey } from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-    /* テストコード 開始 */
-    { key: getKey(), text: "日本語の宿題", done: false },
-    { key: getKey(), text: "reactを勉強する", done: false },
-    { key: getKey(), text: "明日の準備をする", done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  /* テストコード 開始 */
+  //   { key: getKey(), text: "日本語の宿題", done: false },
+  //   { key: getKey(), text: "reactを勉強する", done: false },
+  //   { key: getKey(), text: "明日の準備をする", done: false },
+  //   /* テストコード 終了 */
+  // ]);
 
   const [filter, setFilter] = React.useState("ALL");
 
@@ -60,6 +60,11 @@ function Todo() {
         <TodoItem key={item.key} item={item} onClick={handleCheck} />
       ))}
       <div className="panel-block">{displayItems.length} items</div>
+      <div className="panel-block">
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
+      </div>
     </div>
   );
 }
--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 /* 
   【Storageフック】
@@ -9,22 +9,27 @@ import { useState, useEffect } from 'react';
 　  - localstrageにあるTodoを削除する
 */
 
-const STORAGE_KEY = 'itss-todo';
+const STORAGE_KEY = "itss-todo";
 
 function useStorage() {
-  const [items, setItems] = useState([]);
-　
-　/* 副作用を使う */
+  const [items, setItems] = useState([]); /* 副作用を使う */
   useEffect(() => {
-    
+    const data = localStorage.getItem(STORAGE_KEY);
+    if (!data) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    } else {
+      setItems(JSON.parse(data));
+    }
   }, []);
 
-  const putItems = items => {
-    
+  const putItems = (items) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+    setItems(items);
   };
 
   const clearItems = () => {
-    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    setItems([]);
   };
 
   return [items, putItems, clearItems];
